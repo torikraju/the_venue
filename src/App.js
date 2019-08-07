8a887bbeef97d94efb79f4fc11f67ca73344bc6b
torikul alam <torikul@softitsecurity.com>
2019-08-07 13:36:10 +0600

    Venue INFO component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header';
 import Featured from './components/featured';
+import VenueInfo from './components/venueInfo';
 
 function App() {
   return (
     <div style={{ height: '1500px', backgroundColor: 'cornflowerblue' }}>
       <Header />
       <Featured />
+      <VenueInfo />
     </div>
   );
 }
